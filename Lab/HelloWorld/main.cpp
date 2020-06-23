6e3c295ba3151d36afa174ab76f747df8660beb0
aarreola <anthonyarreola2010@gmail.com>
2020-06-23 11:46:35 -0700

    Signed-off-by: aarreola <anthonyarreola2010@gmail.com>

---
+++ Lab/HelloWorld/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * File:   IntroLab.cpp
+ * Author: Anthony Arreola
+ * Created on June 22, 2020
+ * Purpose:  Hello World! program
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
